5beaf2ca59bfb91a92edc89ed36ea6d02de12617
ZHI-ANG <405169403@qq.com>
2024-01-20 17:31:02 +0800

    update correct license for libmodbus

--- ta/include/my_modbus.h
+++ ta/include/my_modbus.h
@@ -1,22 +1,14 @@
-//-----------------------------------------------------------------------------
-// Copyright 2024 Illinois Advanced Research Center at Singapore
-//
-// This file is part of TEE-PLC Project.
-//
-// TEE-PLC is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// TEE-PLC is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with TEE-PLC.  If not, see <http://www.gnu.org/licenses/>.
-//-----------------------------------------------------------------------------
-
+/*
+ * Modified for TEE-PLC
+ * Illinois Advanced Research Center at Singapore 2024
+ *
+ * Copyright © Stéphane Raimbault <stephane.raimbault@gmail.com>
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ *
+ * This library implements the Modbus protocol.
+ * http://libmodbus.org/
+ */
 
 #ifndef _MY_MODBUS_H_
 #define _MY_MODBUS_H_
