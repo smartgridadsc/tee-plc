90e8c0263f8f38b99372e249f31f6b4b39a474db
ZHI-ANG <405169403@qq.com>
2024-02-02 22:13:35 +0800

    pymodbus clients from enhanced branch

---
+++ pymodbus/mb-tcp-client.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+# Copyright 2024 Illinois Advanced Research Center at Singapore Ltd.
+#
+# This file is part of TEE-PLC.
+#
+# TEE-PLC is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# TEE-PLC is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with TEE-PLC.  If not, see <http://www.gnu.org/licenses/>.
+
+
+from pymodbus.client.sync import ModbusTcpClient
+from time import sleep
+
+client = ModbusTcpClient('127.0.0.1', port=5020)
+# client.write_coil(1, True)
+# result = client.read_coils(1,1)
+# print(result.bits[0])
+# result = client.read_discrete_inputs(1,1)
+# print(result.bits[0])
+
+# client.write_coils(address=0,values=[True, True, True, True],unit=1)
+
+for _ in range(10):
+    #result = client.read_coils(address=0,count=8,unit=1)
+    #print(result.bits)
+
+    result = client.read_input_registers(address=0,count=2,unit=1)
+    print(result)
+
+    sleep(2)
+
+client.close()
---
+++ pymodbus/mb-tls-client.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+# Copyright 2024 Illinois Advanced Research Center at Singapore Ltd.
+#
+# This file is part of TEE-PLC.
+#
+# TEE-PLC is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# TEE-PLC is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with TEE-PLC.  If not, see <http://www.gnu.org/licenses/>.
+
+
+from pymodbus.client.sync import ModbusTlsClient
+from pymodbus.framer.socket_framer import ModbusSocketFramer
+import ssl
+from time import sleep
+
+sslctx = ssl.create_default_context(cafile="certs/ca-cert.pem")
+sslctx.check_hostname = False
+sslctx.options.OP_NO_TLSv1_2
+client = ModbusTlsClient('127.0.0.1', port=8020, sslctx=sslctx,framer=ModbusSocketFramer)
+# client.write_coil(1, True)
+# result = client.read_coils(1,1)
+# print(result.bits[0])
+# result = client.read_discrete_inputs(1,1)
+# print(result.bits[0])
+
+for _ in range(10):
+    # client.write_coils(address=0,values=[True, True, True, True],unit=1)
+    result = client.read_coils(address=0,count=8,unit=1)
+    print(result)
+
+    sleep(2)
+client.close()
